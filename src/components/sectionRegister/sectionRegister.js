12bc774c6d900974246e709fdacd1d0b51a847bd
AleckOstapenko <aleck.ostapenko@gmail.com>
2021-06-25 11:10:38 +0300

    Окончательный вариант

--- src/components/sectionRegister/sectionRegister.js
+++ src/components/sectionRegister/sectionRegister.js
@@ -6,9 +6,7 @@ import Spinner from '../spinner/spinner';
 import Modal from '../modal/modal';
 
 export default class SectionRegister extends Component {
-    
-    // textField = new MDCTextField(document.querySelector('.mdc-text-field'));
-    
+        
     // eslint-disable-next-line no-control-regex
     emailPattern=/^(?:[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
     
